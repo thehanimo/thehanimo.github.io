2fed5870e701afe99b23da121b6c0f7004ece807
thehanimo <hani.mohammed.3@gmail.com>
2019-06-03 20:13:22 +0530

    Updates

---
+++ precache-manifest.b22d760da5cb64eb0236f41de7924cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135ccde4350ddce2ac922c406c16ddbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62e122701760a2d5f9e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b62e122701760a2d5f9e",
+    "url": "/static/js/main.c4870739.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
